b8310ce4f0d14302aa1147f85c561ee197f9a401
krisinniss092191 <kpinniss@gmail.com>
2017-11-15 08:18:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var url =
-"http://api.issuu.com/1_0?action=issuu.documents.list&apiKey=owq32j1gea63hswaii7lzo8ma43nh9n0&access=public&documentSortBy=publishDate&resultOrder=desc&pageSize=30&format=json&signature=6b03703b7653b72c325f0ab5afb625fe";
 
 class Index{
     constructor($http){
